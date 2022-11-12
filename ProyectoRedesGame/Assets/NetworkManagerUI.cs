014bb14cb2b918159a3612a1b75451987e74d9a9
Giovanna Barra <66135504+indonirth@users.noreply.github.com>
2022-11-11 21:00:27 -0600

    Add host address input field

--- ProyectoRedesGame/Assets/NetworkManagerUI.cs
+++ ProyectoRedesGame/Assets/NetworkManagerUI.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 
 public class NetworkManagerUI : MonoBehaviour
 {
     [SerializeField] private Button serverButton;
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
+
+    public string hostIpAddress = "127.0.0.1";
 
     private void Awake()
     {
@@ -19,8 +22,15 @@ public class NetworkManagerUI : MonoBehaviour
             NetworkManager.Singleton.StartHost();
         });
         clientButton.onClick.AddListener(() => {
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
+                hostIpAddress,  // The IP address is a string
+                (ushort)7777 // The port number is an unsigned short
+            );
             NetworkManager.Singleton.StartClient();
         });
     }
 
+    public void ChangeInputIpAddress(string input) {
+        this.hostIpAddress = input;
+    }
 }
