916b494b4e9d0415bc0b5e177e365b5f3373bcc7
Giovanna Barra <66135504+indonirth@users.noreply.github.com>
2022-11-16 00:20:02 -0600

    Update GrabBomb.cs

--- ProyectoRedesGame/Assets/Scripts/GrabBomb.cs
+++ ProyectoRedesGame/Assets/Scripts/GrabBomb.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class GrabBomb : MonoBehaviour
 {    
-    public Transform holdSpot;
-    public LayerMask pickUpMask;
-    public Vector3 Direction { set; get; } //direction of player, where its facing
+    public Transform holdSpot;    
+    public GameObject item;
+    
     private GameObject itemHolding;
-    public GameObject bomb;
 
     void Start() {
-        itemHolding = Instantiate(bomb, holdSpot.position, Quaternion.identity);
+        itemHolding = Instantiate(item, holdSpot.position, Quaternion.identity);
         itemHolding.transform.position = holdSpot.position;
         itemHolding.transform.parent = transform;
         Debug.Log(itemHolding.transform.parent);
@@ -19,26 +18,4 @@ public class GrabBomb : MonoBehaviour
         if(itemHolding.GetComponent<Rigidbody2D>())
             itemHolding.GetComponent<Rigidbody2D>().simulated = false;
     }
-
-    /*void Update() {
-        if(Input.GetKeyDown(KeyCode.E)){
-            if (itemHolding){
-                itemHolding.transform.parent = null;
-                if(itemHolding.GetComponent<Rigidbody2D>())
-                        itemHolding.GetComponent<Rigidbody2D>().simulated = true;
-                itemHolding = null;
-            } else {
-                Collider2D pickUp = Physics2D.OverlapCircle(transform.position + Direction, .8f, pickUpMask);
-                if (pickUp) {
-                    itemHolding = pickUp.gameObject;
-                    itemHolding.transform.position = holdSpot.position;
-                    itemHolding.transform.parent = transform;
-                    Debug.Log(itemHolding.transform.parent);
-                    Debug.Log(transform);
-                    if(itemHolding.GetComponent<Rigidbody2D>())
-                        itemHolding.GetComponent<Rigidbody2D>().simulated = false;
-                }
-            }
-        }
-    }*/
 }
